874ce23ee266dc4a2c69aeea6e66d8bd8b5af8e1
Marc Perales <49318468+Anon0x19@users.noreply.github.com>
2020-06-05 11:30:50 +0200

    Update Utilities.swift

--- TravelTime/Utilities.swift
+++ TravelTime/Utilities.swift
@@ -14,14 +14,14 @@ class Utilities {
     static func styleTextField(_ textfield:UITextField) {
         let bottomLine = CALayer()
         bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
-        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        bottomLine.backgroundColor = UIColor.init(red: 60/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
         
         textfield.borderStyle = .none
         textfield.layer.addSublayer(bottomLine)
     }
     
     static func styleFilledButton(_ button:UIButton) {
-        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.backgroundColor = UIColor.init(red: 60/255, green: 210/255, blue: 210/255, alpha: 1)
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
